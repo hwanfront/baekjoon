2d880198c25cb14eee5c7c5a3f6a3df52b941c4e
hwanfront <kim015jh@gmail.com>
2020-12-31 23:24:27 +0900

    Update: 1996 printer_queue

--- stack_queue/1966printer_queue.js
+++ stack_queue/1966printer_queue.js
@@ -5,82 +5,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let re = null; // 반복
-let doc = []; // 0 : 문서 개수 N, 1 : 몇번째 M
-let imp = [];
-let output = '';
-let isTest = false;
-let isDoc = false;
-
-rl.on("line", function(c) {
-  if (!isTest) {
-    re = parseInt(c);
-    isTest = !isTest;
-  } 
-  else {
-    if(!isDoc) {
-      doc = c.split(' ').map((e) => parseInt(e));
-      isDoc = !isDoc;
-    }
-    else {
-      imp = c.split(' ').map((e) => parseInt(e));
-      let docArr = setDocArr(imp);
-      let maxImp = maxImportance(docArr);
-      let res = 1;
-      while(1) {
-        if (docArr[0][1] == maxImp && docArr[0][0] == doc[1]) {
-          output = output + res + '\n';
-          break;
-        }
-        else if (docArr[0][1] == maxImp) {
-          docArr.shift();
-          maxImp = maxImportance(docArr);
-          res++;
-        }
-        else {
-          docArr.push(docArr.shift());
-        }
-      }
-
-      isDoc = !isDoc;
-      re--;
-    }
-    
-    if (re == 0) {
-      rl.close();
-    }
-  }
-}).on("close", function() {
-  console.log(output);
-  process.exit();
-});
-
-function setDocArr (arr) {
-  let res = [];
-  for (let i = 0; i < arr.length; i++) {
-    res.push([i,arr[i]]);
-  }
-  return res;
-}
-
-function maxImportance (arr) {
-  let max = arr[0][1];
-  for (let i = 1; i < arr.length; i++) {
-    if(max < arr[i][1]) {
-      max = arr[i][1];
-    }
-  }
-  return max;
-}
-
-
-
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 let testcase = null;
 let M = -1;
 let output = '';
@@ -91,7 +15,6 @@ rl.on("line", function(c) {
   } 
   else {  // 반복 입력
     if(M === -1) {
-      let doc = c.split(' ');
       M = parseInt(c.split(' ')[1]);
     }
     else {
