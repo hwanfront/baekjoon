52fbb813f7ab3eb826c9c4aa177de32bc9acd477
hwanfront <kim015jh@gmail.com>
2021-01-01 23:19:26 +0900

    fix: fix 'slice()' so that 'use index'

--- algorithm/MergeSort.js
+++ algorithm/MergeSort.js
@@ -1,27 +1,30 @@
+// 음수는 parseInt 필수
 function mergeSort(arr) {
     if (arr.length === 1) {
-        return arr;
+      return arr;
     }
-
-    const mid = Math.floor(arr.length / 2);
-    const left = arr.slice(0, mid);
-    const right = arr.slice(mid);
-
+    
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+  
     return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-
+  }
+  
+  function merge(left, right) {
     let result = [];
-
-    while (left.length && right.length) {
-        +left[0] < +right[0] ? result.push(left.shift()) : result.push(right.shift());
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while(leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
     }
-    while (left.length) {
-        result.push(left.shift());
-    }
-    while (right.length) {
-        result.push(right.shift());
-    }
-    return result;
-}
+  
+    return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+  }
