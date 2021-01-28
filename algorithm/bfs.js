4b6d30c40e954adf2198acdbaf05bfd86591af43
hwanfront <kim015jh@gmail.com>
2021-01-28 23:15:19 +0900

    feat : bfs.js

---
+++ algorithm/bfs.js
@@ -0,0 +1,15 @@
+const bfs = (num) => {
+    const queue = [];
+    queue.push(num);
+    visited[num] = true;
+    while (queue.length) {
+        const v = queue.shift();
+        res.push(v);
+        for (let i = 0; i < graph[v].length; i++) {
+            if (!visited[graph[v][i]]) {
+                queue.push(graph[v][i]);
+                visited[graph[v][i]] = true;
+            }
+        }
+    }
+};
