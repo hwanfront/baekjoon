9340f142d361b2379c21f4632ac01ba4c307dada
hwanfront <kim015jh@gmail.com>
2021-03-07 21:06:18 +0900

    Add : ERROR 2263 tree StackSizeExceeded

---
+++ tree/2263.js
@@ -0,0 +1,36 @@
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const input = [
+    '4',
+    '4 1 2 3',
+    '4 1 3 2',
+]
+// const input = [
+// '6',
+// '5 2 4 1 6 3',
+// '5 4 2 6 3 1',
+// ];
+
+// const input = [
+//   '8',
+//   '8 4 2 5 1 6 3 7',
+//   '8 4 5 2 6 7 3 1',
+// ]
+
+const n = Number(input[0]);
+const inOrderArr = input[1].split(" ").map((e) => Number(e));
+const postOrderArr = input[2].split(" ").map((e) => Number(e));
+let result = "";
+
+const preOrder = (inStart, inEnd, postStart, postEnd) => {
+    if (postEnd < postStart || inEnd < inStart) return;
+    const root = postOrderArr[postEnd];
+    const inOrderRootIdx = inOrderArr.indexOf(root);
+    result += root + " ";
+    preOrder(inStart, inOrderRootIdx - 1, postStart, postStart + inOrderRootIdx - 1 - inStart);
+    preOrder(inOrderRootIdx + 1, inEnd, postStart + (inOrderRootIdx - inStart), postEnd - 1);
+}
+
+preOrder(0, n - 1, 0, n - 1);
+
+console.log(result.trim());
