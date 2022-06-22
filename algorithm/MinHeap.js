16d5311807478e9744b372b984593f7c41ea8831
hwanfront <kim015jh@gmail.com>
2022-06-22 22:52:24 +0900

    fix

--- algorithm/MinHeap.js
+++ algorithm/MinHeap.js
@@ -1,73 +1,56 @@
 class MinHeap {
   constructor() {
-    this.node = [];
+    this.heap = [];
   }
 
-  insert(value) {
-    this.node.push(value);
-    this.bubbleUp();
+  getLength() {
+    return this.heap.length;
   }
 
-  bubbleUp(index = this.node.length - 1) {
-    if (index < 1) return;
-
-    const currentNode = this.node[index];
-    const parentIndex = Math.floor((index - 1) / 2);
-    const parentNode = this.node[parentIndex];
-    if (parentNode.cost <= currentNode.cost) return;
-
-    this.node[index] = parentNode;
-    this.node[parentIndex] = currentNode;
-    index = parentIndex;
-    this.bubbleUp(index);
-  }
-
-  extract() {
-    const min = this.node[0];
-    if (this.node.length === 1) {
-      this.node.pop();
-      return min;
+  push(node) {
+    this.heap.push(node);
+    let i = this.heap.length - 1;
+    let parentI = Math.floor((i - 1) / 2);
+    while (i > 0 && this.heap[parentI] > this.heap[i]) {
+      this.swap(i, parentI);
+      i = parentI;
+      parentI = Math.floor((i - 1) / 2);
     }
-    this.node[0] = this.node.pop();
-    this.trickleDown();
-    return min;
   }
 
-  trickleDown(index = 0) {
-    const leftChildIndex = 2 * index + 1;
-    const rightChildIndex = 2 * index + 2;
-    const length = this.node.length;
-    let minimum = index;
+  pop() {
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
 
-    if (!this.node[rightChildIndex] && !this.node[leftChildIndex]) return;
-    if (!this.node[rightChildIndex]) {
-      if (this.node[leftChildIndex].cost < this.node[minimum].cost) {
-        minimum = leftChildIndex;
+    const result = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    let i = 0;
+    while (true) {
+      const leftI = i * 2 + 1,
+        rightI = i * 2 + 2;
+      if (leftI >= this.heap.size) {
+        break;
       }
-      return;
-    }
-
-    if (this.node[leftChildIndex].cost > this.node[rightChildIndex].cost) {
-      if (
-        rightChildIndex <= length &&
-        this.node[rightChildIndex].cost < this.node[minimum].cost
-      ) {
-        minimum = rightChildIndex;
+      let nextI = i;
+      if (this.heap[nextI] > this.heap[leftI]) {
+        nextI = leftI;
       }
-    } else {
-      if (
-        leftChildIndex <= length &&
-        this.node[leftChildIndex].cost < this.node[minimum].cost
-      ) {
-        minimum = leftChildIndex;
+      if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
+        nextI = rightI;
       }
+      if (nextI === i) {
+        break;
+      }
+      this.swap(i, nextI);
+      i = nextI;
     }
+    return result;
+  }
 
-    if (minimum !== index) {
-      let t = this.node[minimum];
-      this.node[minimum] = this.node[index];
-      this.node[index] = t;
-      this.trickleDown(minimum);
-    }
+  swap(a, b) {
+    const temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
   }
 }
